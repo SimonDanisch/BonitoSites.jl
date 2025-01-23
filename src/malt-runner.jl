2eecc2c6e14da39aac29e2cbbdabfd3963381a92
SimonDanisch <sdanisch@protonmail.com>
2025-01-23 12:05:05 +0100

    grab images permanantly without another download at build via hashed file key for url

--- src/malt-runner.jl
+++ src/malt-runner.jl
@@ -6,16 +6,66 @@ struct ToSVG
     obj::Any
 end
 
+using SHA, FileIO
+
+function get_file_ext(sym::Symbol)
+    # May error if sym not found. If no error it should return (how_to_detect, ext::Union{Vector{String}, String})
+    extensions = FileIO.info(sym)[2]
+    if extensions isa AbstractVector
+        return first(extensions)
+    elseif extensions isa AbstractString
+        return extensions
+    else
+        error("Unexpected return type for `FileIO.info`: $(extensions)")
+    end
+end
+function get_file_ext(bytes::Vector{UInt8})
+    format = FileIO.query(IOBuffer(bytes))
+    return get_file_ext(FileIO.formatname(format))
+end
+
+function find_file_without_ext(folder, file)
+    for f in readdir(folder)
+        if startswith(f, file)
+            return joinpath(folder, f)
+        end
+    end
+    return nothing
+end
+
+function grab_image_from_url(folder, url)
+    file_mime = Bonito.file_mimetype(url)
+    # returns "bin" if no ending found
+    file_ext = Bonito.HTTPServer.mimetype_to_extension(file_mime)
+    url_hash = bytes2hex(sha1(abspath(url))) # use as unique file key
+    img_folder = joinpath(folder, "images")
+    !isdir(img_folder) && mkpath(img_folder)
+
+    if file_ext == "bin"
+        path = find_file_without_ext(img_folder, url_hash)
+        if isnothing(path)
+            # we have to download the bytes if not already downladed
+            bytes = Bonito.HTTP.get(url).body
+            file_ext = get_file_ext(bytes)
+            path = joinpath(img_folder, url_hash * file_ext) # comes with '.' already
+            !isfile(path) && write(path, bytes)
+        end
+    else
+        path = joinpath(img_folder, url_hash * "." * file_ext)
+        if !isfile(path)
+            Downloads.download(img.url, path)
+        end
+    end
+    return path
+end
+
 function rewrite_img(session, current_folder, img)
     imgurl = img.url
     if isfile(img.url)
         imgurl = Bonito.url(session, Bonito.Asset(img.url))
     elseif startswith(img.url, "http")
-        file_mime = Bonito.file_mimetype(img.url)
-        io = IOBuffer()
-        Downloads.download(img.url, io)
-        asset = Bonito.BinaryAsset(take!(io), file_mime)
-        imgurl = Bonito.url(session, asset)
+        path = grab_image_from_url(current_folder, img.url)
+        imgurl = Bonito.url(session, Bonito.Asset(path))
     elseif startswith(img.url, "./")
         fileurl = img.url[3:end]
         file = Bonito.to_unix_path(joinpath(current_folder, fileurl))
